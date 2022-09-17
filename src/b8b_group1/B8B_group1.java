a0f6ec9579d88a8d49c27d5197525a189ee8bebd
Raghad-Orri <Raghadorri@gmail.com>
2022-09-17 19:02:26 +0300

    Raghad commit

--- src/b8b_group1/B8B_group1.java
+++ src/b8b_group1/B8B_group1.java
@@ -17,7 +17,7 @@ public class B8B_group1 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hi, I am Nora Bajabaa");
-        System.out.println("Hi, I am Raghad Orri");
+        System.out.println("Hi, I am Raghad Orri*");
     }
     
 }
